baf62f419cc17e3c5f085b43dfc1b8c7891d9f44
PadelPlayers <padelplayers@correo.com>
2026-03-17 19:48:50 +0100

    can

--- app.js
+++ app.js
@@ -20,3 +20,57 @@ alert(e.message);
 });
 
 }
+
+function guardarPerfil(){
+
+  document.getElementById("msgPerfil").innerText = "";
+
+  const user = auth.currentUser;
+
+  if(!user){
+  document.getElementById("msgPerfil").innerText = "Error de sesión";
+  return;
+}
+
+  const nombre = document.getElementById("nombre").value.trim().toLowerCase();
+  const sexo = document.getElementById("sexo").value;
+  const nivel = document.getElementById("nivelManual").value;
+
+  const mano = document.getElementById("mano").value;
+const posicion = document.getElementById("posicion").value;
+
+  if(!nombre || !nivel || !mano || !posicion){
+    document.getElementById("msgPerfil").innerText = "Completa los campos";
+    return;
+  }
+
+  db.collection("usuarios")
+    .where("nombre", "==", nombre)
+    .get()
+    .then(query=>{
+
+  console.log("Usuarios encontrados:", query.size);
+
+  if(!query.empty){
+    document.getElementById("msgPerfil").innerText = "Nombre ya en uso";
+    return;
+  }
+
+      db.collection("usuarios").doc(user.uid).set({
+        nombre: nombre,
+        sexo: sexo,
+        nivel: parseFloat(nivel),
+        mano: mano,
+        posicion: posicion
+      })
+      .then(()=>{
+        mostrar("menu");
+      });
+
+    })
+    .catch(e=>{
+      console.log(e);
+      document.getElementById("msgPerfil").innerText = "Error";
+    });
+
+}
