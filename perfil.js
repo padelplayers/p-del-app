676905ee63cb9d960bf89e80d0c783b153579254
PadelPlayers <padelplayers@correo.com>
2026-03-30 02:27:49 +0200

    act

--- perfil.js
+++ perfil.js
@@ -103,25 +103,7 @@ function toggleSeguir(){
 
     }
 
-    Promise.all(promesas).then(() => {
-
-  const seguidoresElem = document.getElementById("seguidores");
-  const seguidosElem = document.getElementById("seguidos");
-
-  let seguidores = parseInt(seguidoresElem.innerText) || 0;
-  let seguidos = parseInt(seguidosElem.innerText) || 0;
-
-  if (sigo) {
-    // estabas siguiendo → ahora dejas de seguir
-    seguidoresElem.innerText = seguidores - 1;
-    seguidosElem.innerText = seguidos - 1;
-  } else {
-    // no seguías → ahora sigues
-    seguidoresElem.innerText = seguidores + 1;
-    seguidosElem.innerText = seguidos + 1;
-  }
-
-});
+    
 
   });
 
