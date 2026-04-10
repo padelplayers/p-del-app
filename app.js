d176aee813fe22d57765809080da21eeea0d84f4
PadelPlayers <padelplayers@correo.com>
2026-04-10 18:57:28 +0200

    act

--- app.js
+++ app.js
@@ -454,7 +454,10 @@ function cargarPistas() {
         div.className = "cardPista";
 
         div.innerHTML =
-          (data.fotoPista ? '<img src="' + data.fotoPista + '" style="width:100%; border-radius:10px; margin-bottom:8px;">' : '') +
+         (data.fotoPista || data.foto ? 
+  '<img src="' + (data.fotoPista || data.foto) + '" style="width:100%; border-radius:10px; margin-bottom:8px;">' 
+  : ''
+)
           "<strong>" + (data.nombre || "") + "</strong><br>" +
           (data.localidad || "") + "<br>" +
           (data.tipo || "") + "<br>" +
