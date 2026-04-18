b3f8fb20a26f699ef361b9a7dfc9b43deb7e262d
JUanvi <juanvicp81@gmail.com>
2026-04-18 22:44:38 +0200

    act

--- app.js
+++ app.js
@@ -363,7 +363,9 @@ function mostrar(seccion){
     window.pistasCargadas = true;
   }
   if (seccion === "partidas") {
-  cargarPistasParaPartida();
+  setTimeout(() => {
+    cargarPistasParaPartida();
+  }, 100);
 }
 }
 
