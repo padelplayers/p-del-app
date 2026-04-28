3da424f2911fa021751f39b53148791f081f93c5
JUanvi <juanvicp81@gmail.com>
2026-04-28 22:00:33 +0200

    act

--- partidas.js
+++ partidas.js
@@ -275,7 +275,7 @@ function cargarPartidas() {
       for (var i = 0; i < 2; i++) {
         pintarJugador(p.reservas[i] || null, "r" + (i + 1) + "_" + doc.id);
       }
-
+console.log("CREADOR ID:", p.creador);
       if (p.creador) {
   db.collection("usuarios").doc(p.creador).get().then(docUser => {
     if (docUser.exists) {
