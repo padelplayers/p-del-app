7e41e45a060f1e41b1629ef9a7321ed5ac85df00
JUanvi <juanvicp81@gmail.com>
2026-06-21 06:38:36 +0200

    act

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = "padel-players-morvedre-v99";
+const CACHE_NAME = "padel-players-morvedre-v100";
 const APP_SHELL = [
   "./manifest.json?v=3",
-  "./styles.css?v=50",
+  "./styles.css?v=51",
   "./logros.js?v=5",
   "./perfil.js?v=35",
   "./jugadores.js?v=4",
@@ -10,7 +10,7 @@ const APP_SHELL = [
   "./notifications.js?v=13",
   "./estadisticas.js?v=6",
   "./admin-chat-cleanup.js?v=6",
-  "./pwa.js?v=58",
+  "./pwa.js?v=59",
   "./app.js?v=24",
   "./logo.png",
   "./icon-192-v2.png",
